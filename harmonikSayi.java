7c8f7f6ca9a392f155e616de0a818d9dc1bc0a7f
sevdenise0529 <101528396+sevdenise0529@users.noreply.github.com>
2022-12-27 12:44:27 +0300

    Create harmonikSayi.java

---
+++ harmonikSayi.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main{
+  public static void main(String [] args){
+    Scanner input = new Scanner(System.in);
+    System.out.print("N sayisini giriniz: ");
+    int n = input.nextInt();
+    double result = 0;
+    
+    for (double i = 0; i<= n; i++){
+    result += (1/i);
+    }
+    // 1.Döngü, i=1; result = 0 + (1/1) = 1;
+    // 2.Döngü, i=2; result = 1 + (1/2) = 1.5;
+    System.out.println("Sonuc: " + result);
+  }
+    
